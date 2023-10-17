907a6cd752216c934bb6a78e246435a669843464
syamn132 <ssrn132@gmail.com>
2023-10-18 01:05:48 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Item from './MyItem';
+// import Counter from './MyCounter';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <Item />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/MyItem.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import './App.css';
+import originalImage from './bulboff.png';
+import replacedImage from './bulbon.png';
+
+function Item() {
+  const [showText, setShowText] = useState(false);
+  const [count, setCount] = useState(0);
+  const [imageSource, setImageSource] = useState(originalImage);
+  const imageAltText = "Image";
+
+  const handleClick = () => {
+    setShowText(!showText); 
+    setCount(count+1);
+    setImageSource(imageSource === originalImage ? replacedImage : originalImage);
+  };
+
+  return (
+    <div>
+        <img src={imageSource} alt = {imageAltText} onClick={handleClick} width="150px"/>
+        {showText && <p className='greetings'>Hello World</p>}
+        <p className='cnt' >{count}</p>
+        <button className='button' onClick={handleClick}>Click Me</button>
+    </div>
+  );
+}
+
+export default Item;
